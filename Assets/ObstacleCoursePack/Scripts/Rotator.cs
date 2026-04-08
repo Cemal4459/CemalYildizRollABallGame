c3b807eb42923bc5836b48951c734f7348cfee0c
Cemal4459 <cemalyildiz263@gmail.com>
2026-04-08 05:23:52 +0300

    final: Complete level design with Obstacle Course Pack, Skybox, and AI Navigation

---
+++ Assets/ObstacleCoursePack/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+
+    void Update()
+    {
+        // Mavi halka (Z ekseni) etrafında döner
+        transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+    }
+}
